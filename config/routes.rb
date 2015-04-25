0a3541fc0dac7956a82d2f92cbffc48af218e05c
Woodson Delhia <woodsondelhia@hotmail.com>
2015-04-25 09:42:38 -0400

    Added devise model and micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  devise_for :users
   root 'staticpages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
